aba7beb78ef5cec3aa3b53c9748f2c3e703ebbb3
rabinpaul2022 <117923453+rabinpaul2022@users.noreply.github.com>
2022-11-18 18:05:27 +0530

    2375 port changes

--- app/app.go
+++ app/app.go
@@ -25,5 +25,5 @@ func Start() {
 	router.HandleFunc("/customers", ch.getallcustomers).Methods(http.MethodGet)
 	//mux.HandleFunc("/newcustomer", createCustomer).Methods(http.MethodPost)
 
-	log.Fatal(http.ListenAndServe("localhost:8333", router))
+	log.Fatal(http.ListenAndServe("localhost:2375", router))
 }
